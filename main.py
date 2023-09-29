1f65127b807dc3a0ab5ef0495594eef84d3b746d
mrinstance <142258555+mrinstance@users.noreply.github.com>
2023-09-29 15:04:41 -0600

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+import pygame
+
+# Inicializar Pygame
+pygame.init()
+
+# Configuración de la ventana
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Asteroids Clone")
+
+# Bucle principal del juego
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Lógica del juego y dibujo de objetos irán aquí
+
+    pygame.display.update()
+
+# Salir de Pygame
+pygame.quit()
 
